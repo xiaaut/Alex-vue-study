2f50cfa295828ea13a2188138d6513034a6b21ec
xia <836807919@qq.com>
2026-01-31 01:30:52 +0800

    学完 v-model

--- part1/section2/starter/vanilla.js
+++
@@ -1,6 +0,0 @@
-const inputEl = document.querySelector(".input-text");
-const pEl = document.querySelector(".p-text");
-
-inputEl.addEventListener("input", (e) => {
-    pEl.textContent = e.target.value;
-});
