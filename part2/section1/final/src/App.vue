1f05c28eed4e1f301789bcdb83813453aa8aa852
xia <836807919@qq.com>
2026-04-24 22:01:33 +0800

    脚手架基础使用

--- part2/section1/final/src/App.vue
+++ part2/section1/final/src/App.vue
@@ -1,62 +1,68 @@
 <template>
-        <TogglePurple :togglePurple="togglePurple"></TogglePurple>
-        <select v-model="circleColor">
-            <option value="">White</option>
-            <option value="text-black">Black</option>
-            <option value="text-orange">Orange</option>
-        </select>
+  <TogglePurple :togglePurple="togglePurple"></TogglePurple>
+  <select v-model="circleColor">
+    <option value="">White</option>
+    <option value="text-black">Black</option>
+    <option value="text-orange">Orange</option>
+  </select>
 
-        <label>
-            Circle Size
-            <input type="number" v-model="circleSize" />
-        </label>
+  <label>
+    Circle Size
+    <input type="number" v-model="circleSize" />
+  </label>
 
-        <label>
-            Circle Rotate
-            <input type="number" v-model="circleAngle" />
-        </label>
-        <div class="circle" :class="[circleClass, circleColor]" :style="circleStyle">Hi!</div>
+  <label>
+    Circle Rotate
+    <input type="number" v-model="circleAngle" />
+  </label>
+  <div class="circle" :class="[circleClass, circleColor]" :style="circleStyle">Hi!</div>
 </template>
 
 <script>
-
-import TogglePurple from './components/TogglePurple.vue';
+// import TogglePurple from './components/TogglePurple.vue';
 export default {
-    components: {
-        TogglePurple,
-    },
-    data () {
-        return {
-            isPurple: false,
-            circleColor:'',
-            circleSize: 200,
-            circleAngle: '',
-        };
-    },
+  //   components: {
+  //     TogglePurple,
+  //   },
+  data() {
+    return {
+      isPurple: false,
+      circleColor: '',
+      circleSize: 200,
+      circleAngle: '',
+    };
+  },
 
-    methods: {
-        togglePurple() {
-            this.isPurple = !this.isPurple;
-        }
+  methods: {
+    togglePurple() {
+      this.isPurple = !this.isPurple;
     },
+  },
 
-    computed : {
-        circleClass() {
-            return this.isPurple ? 'purple' : '';
-        },
-        circleStyle() {
-            return {
-                width: this.circleSize + 'px',
-                height: this.circleSize + 'px',
-                lineHeight: `${this.circleSize}px`,
-                transform: `rotate(${this.circleAngle}deg)`,
-            }
-        }
-    }
+  computed: {
+    circleClass() {
+      return this.isPurple ? 'purple' : '';
+    },
+    circleStyle() {
+      return {
+        width: this.circleSize + 'px',
+        height: this.circleSize + 'px',
+        lineHeight: `${this.circleSize}px`,
+        transform: `rotate(${this.circleAngle}deg)`,
+      };
+    },
+  },
 };
 </script>
 
-<style scoped>
-
-
+<style>
+.purple {
+  background-color: #767dea;
+}
+.text-black {
+  color: #424242;
+}
+.text-orange {
+  color: #ffc26f;
+}
 </style>
