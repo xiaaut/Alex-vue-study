48dfd5cf2aa69f7bf231a623469cf1874dd4499e
xia <836807919@qq.com>
2026-05-08 21:34:32 +0800

    修复加载器问题

--- part3/hot-news/final-08-spinner/src/ui/Loading.vue
+++ part3/hot-news/final-08-spinner/src/ui/Loading.vue
@@ -1,1 +1,7 @@
-<template><van-loading type="spinner" size="24px" color="#1989fa" /></template>
+<template><van-loading type="spinner" size="48px" color="#1989fa" /></template>
+<style scoped>
+* {
+  margin-top: 30%;
+  text-align: center;
+}
+</style>
--- part3/hot-news/final-08-spinner/src/ui/NewList.vue
+++ part3/hot-news/final-08-spinner/src/ui/NewList.vue
@@ -1,6 +1,5 @@
 <template>
-  <loading v-if="!news.list"></loading>
-
+  <loading v-if="!news.length"></loading>
   <van-card v-else v-for="currentNew in news" :price="currentNew.source" :desc="currentNew.desc" :title="currentNew.title" :thumb="currentNew.imgList[0]" currency="">
     <template #footer>
       <van-tag size="small" type="primary">收藏</van-tag>
